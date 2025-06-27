cfa3ac4ac788c128ddb0da993f733345b4a469c7
Daniel Neves <73949890+Dan7Arievlis@users.noreply.github.com>
2025-06-27 10:52:33 -0300

    First commit

---
+++ src/main/java/com/danielsilveira/jpa_springboot_course/JpaSpringbootCourseApplication.java
@@ -0,0 +1,13 @@
+package com.danielsilveira.jpa_springboot_course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaSpringbootCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaSpringbootCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danielsilveira/jpa_springboot_course/JpaSpringbootCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielsilveira.jpa_springboot_course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaSpringbootCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
