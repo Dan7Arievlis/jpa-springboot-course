626c2d0cb7074204abbbaf28b3d6dfc08bd17399
Daniel Neves <73949890+Dan7Arievlis@users.noreply.github.com>
2025-06-27 12:26:01 -0300

    Service Layer, component registration

---
+++ src/main/java/com/danielsilveira/jpa_springboot_course/services/UserService.java
@@ -0,0 +1,23 @@
+package com.danielsilveira.jpa_springboot_course.services;
+
+import com.danielsilveira.jpa_springboot_course.entities.User;
+import com.danielsilveira.jpa_springboot_course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    @SuppressWarnings("OptionalGetWithoutIsPresent")
+    public User findById(Long id) {
+        return repository.findById(id).get();
+    }
+}
