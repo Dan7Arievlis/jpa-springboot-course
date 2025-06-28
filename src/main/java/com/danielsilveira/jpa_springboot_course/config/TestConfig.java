595f972f421c0bb6df77ffeeaeddd2e604f8b0d5
Daniel Neves <73949890+Dan7Arievlis@users.noreply.github.com>
2025-06-28 00:45:42 -0300

    Many to many association with Join Table

--- src/main/java/com/danielsilveira/jpa_springboot_course/config/TestConfig.java
+++ src/main/java/com/danielsilveira/jpa_springboot_course/config/TestConfig.java
@@ -31,13 +31,6 @@ public class TestConfig implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        User user1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "12345678");
-        User user2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "12345678");
-
-        Order order1 = new Order(null, Instant.parse("2025-05-20T19:53:07Z"), OrderStatus.PAID, user1);
-        Order order2 = new Order(null, Instant.parse("2024-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, user2);
-        Order order3 = new Order(null, Instant.parse("2025-02-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, user1);
-
         Category category1 = new Category(null, "Electronics");
         Category category2 = new Category(null, "Books");
         Category category3 = new Category(null, "Computers");
@@ -48,9 +41,26 @@ public class TestConfig implements CommandLineRunner {
         Product product4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
         Product product5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 
-        userRepository.saveAll(List.of(user1, user2));
-        orderRepository.saveAll(List.of(order1, order2, order3));
         categoryRepository.saveAll(List.of(category1, category2, category3));
         productRepository.saveAll(List.of(product1, product2, product3, product4, product5));
+
+        product1.getCategories().add(category2);
+        product2.getCategories().add(category1);
+        product2.getCategories().add(category3);
+        product3.getCategories().add(category3);
+        product4.getCategories().add(category3);
+        product5.getCategories().add(category2);
+
+        productRepository.saveAll(List.of(product1, product2, product3, product4, product5));
+
+        User user1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "12345678");
+        User user2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "12345678");
+
+        Order order1 = new Order(null, Instant.parse("2025-05-20T19:53:07Z"), OrderStatus.PAID, user1);
+        Order order2 = new Order(null, Instant.parse("2024-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, user2);
+        Order order3 = new Order(null, Instant.parse("2025-02-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, user1);
+
+        userRepository.saveAll(List.of(user1, user2));
+        orderRepository.saveAll(List.of(order1, order2, order3));
     }
 }
