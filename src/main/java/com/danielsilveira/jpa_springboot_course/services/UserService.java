7eab21c3bf7e91948edb85e3b505c6762ff31cbd
Daniel Neves <73949890+Dan7Arievlis@users.noreply.github.com>
2025-06-29 11:43:29 -0300

    ExceptionHandler - FindById

--- src/main/java/com/danielsilveira/jpa_springboot_course/services/UserService.java
+++ src/main/java/com/danielsilveira/jpa_springboot_course/services/UserService.java
@@ -2,6 +2,7 @@ package com.danielsilveira.jpa_springboot_course.services;
 
 import com.danielsilveira.jpa_springboot_course.entities.User;
 import com.danielsilveira.jpa_springboot_course.repositories.UserRepository;
+import com.danielsilveira.jpa_springboot_course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +19,7 @@ public class UserService {
 
     @SuppressWarnings("OptionalGetWithoutIsPresent")
     public User findById(Long id) {
-        return repository.findById(id).get();
+        return repository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User user) {
