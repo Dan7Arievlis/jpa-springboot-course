989e79cc8408736e6b4cdc0528638307f9b73ae6
Daniel Neves <73949890+Dan7Arievlis@users.noreply.github.com>
2025-06-29 11:53:29 -0300

    Exception handling - delete

--- src/main/java/com/danielsilveira/jpa_springboot_course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/danielsilveira/jpa_springboot_course/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.danielsilveira.jpa_springboot_course.resources.exceptions;
 
+import com.danielsilveira.jpa_springboot_course.services.exceptions.DatabaseException;
 import com.danielsilveira.jpa_springboot_course.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -8,13 +9,22 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.Instant;
+import java.util.zip.DataFormatException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<StandardError> resorceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+    public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
         String error = "Resource not found.";
         HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+        String error = "Database error.";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
